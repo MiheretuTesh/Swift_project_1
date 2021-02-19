425a28b894131ef75a5ceb9d6da342787de9048b
Kaleab Girma <se.kaleab.girma@gmail.com>
2021-02-20 01:50:06 +0300

    username check for log in works

--- scr/assets/script/app.js
+++ scr/assets/script/app.js
@@ -2,20 +2,25 @@ import Database from './database.js';
 const DB = new Database()
 //Create Account    
 const register = document.querySelector("#register")
-const fullName = document.querySelector("#fullNameID").value
-const username = document.querySelector("#unameID").value
-const password = document.querySelector("#passID").value
-const birthDay = document.querySelector("#birthDay").value
+const fullName = document.querySelector("#fullNameID")
+const uname_register = document.querySelector("#unameID")
+const password_register = document.querySelector("#passID")
+const birthDay = document.querySelector("#birthDay")
+//Log in
+const login = document.querySelector("#login")
+const username = document.querySelector("#username").value
+const password = document.querySelector("#password").value
 
 
-
-
-
+//login
+login.addEventListener('submit', (e) => {
+    e.preventDefault();
+    DB.login(username, password)
+})
+//create account
 register.addEventListener('submit', (e) => {
     e.preventDefault();
-    DB.createAccount(fullName,username,password,birthDay);
+    DB.createAccount(fullName.value,uname_register.value,password_register.value,birthDay.value);
 })
 
 
-
-//
