0077404a2ba431a43648163e1079274b09c61750
Kaleab Girma <se.kaleab.girma@gmail.com>
2021-02-20 00:39:38 +0300

    create account works by importing from Database and working on app.js

--- scr/assets/script/app.js
+++ scr/assets/script/app.js
@@ -1,12 +1,21 @@
 import Database from './database.js';
-
+const DB = new Database()
 //Create Account    
-const registerBtn = document.querySelector("#registerBtn")
+const register = document.querySelector("#register")
 const fullName = document.querySelector("#fullNameID").value
 const username = document.querySelector("#unameID").value
 const password = document.querySelector("#passID").value
 const birthDay = document.querySelector("#birthDay").value
 
 
-registerBtn.addEventListener('click', () => Database.createAccount())
 
+
+
+register.addEventListener('submit', (e) => {
+    e.preventDefault();
+    DB.createAccount(fullName,username,password,birthDay);
+})
+
+
+
+//
